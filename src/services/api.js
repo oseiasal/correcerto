84c776afffae4f12fc41bf11ca7e9f907718fedf
Oseias <oseias.nascimento@kainos.com.br>
2021-01-17 18:13:05 -0300

    adicionado novos endpoints

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,29 @@
 import axios from "axios";
 
+const url = "http://simpleware.ddns.net:1880";
+
 export function getUsers() {
   const options = {
     method: "GET",
-    url: "http://simpleware.ddns.net:1880/nome",
+    url: url + "/usuario",
+  };
+
+  return axios.request(options);
+}
+
+export function getCurriculum() {
+  const options = {
+    method: "GET",
+    url: url + "/curriculo",
+  };
+
+  return axios.request(options);
+}
+
+export function getDesafios() {
+  const options = {
+    method: "GET",
+    url: url + "/desafio",
   };
 
   return axios.request(options);
