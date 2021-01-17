acfecaaaa8fb68ce608a3924354c07e26398130a
Oseias <oseias.nascimento@kainos.com.br>
2021-01-17 09:35:36 -0300

    correção erro drawer

--- src/screens/Main/Perfil/index.jsx
+++ src/screens/Main/Perfil/index.jsx
@@ -178,7 +178,7 @@ const GraphImage = styled.Image`
   width: 150px;
 `;
 
-export default function () {
+export default function ({ openDrawer }) {
   const name = "Pedro Almeida Nogueira";
   return (
     <>
