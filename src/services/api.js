4e8abf08c3363289c7526371c0b9d9820e80ef32
Oseias <oseias.nascimento@kainos.com.br>
2021-01-17 17:35:36 -0300

    adicionado axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,10 @@
-export function getUsers() {}
+import axios from "axios";
+
+export function getUsers() {
+  const options = {
+    method: "GET",
+    url: "http://simpleware.ddns.net:1880/nome",
+  };
+
+  return axios.request(options);
+}
